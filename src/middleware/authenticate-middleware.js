2677f159f3e033427ecd82c25c2d77ed8783920f
unknown <rijal.1344@gmail.com>
2024-03-03 10:59:42 +0700

    refactor: flow authentication using jwt

--- src/middleware/authenticate-middleware.js
+++ src/middleware/authenticate-middleware.js
@@ -1,29 +1,40 @@
 import { prismaClient } from "../app/database.js"
+import Jwt from "jsonwebtoken"
 
 const authMiddleware = async (request, response, next) => {
     const token = request.get('Authorization')
+    
     if(!token){
         return response.status(401).json({
             data: 'unauthorized'
         }).end()
     }
-    else{
-        const user = await prismaClient.user.findFirst({
-            where: {
-                token: token
-            }
-        })
 
-        if(!user){
+    Jwt.verify(token, 'swefijlzc22@#()33vsd', async function(err, decoded) {
+        if(err){
             return response.status(401).json({
-                data: 'unauthorized'
+                errorToken: err.name,
+                errorMessage: err.message
             }).end()
         }
         else{
-            request.user = user
-            return next()
+            const user = await prismaClient.user.findFirst({
+                where: {
+                    token: token
+                }
+            })
+    
+            if(!user){
+                return response.status(401).json({
+                    data: 'unauthorized'
+                }).end()
+            }
+            else{
+                request.user = user
+                return next()
+            }
         }
-    }
+    });
 }
 
 export {
